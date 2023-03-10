21b8ec49f2a0a8ca13ac4c90b351fa7fdc95ec84
CarollAlmeida <carolinealmeida0811@gmail.com>
2023-03-10 09:08:43 -0300

    Commit initial

---
+++ app/src/main/java/com/example/meuprimeiroprojeto/ResultActivity.kt
@@ -0,0 +1,42 @@
+package com.example.meuprimeiroprojeto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.TextView
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val tvresult = findViewById<TextView>(R.id.textview_result)
+        val tvClassificacao = findViewById<TextView>(R.id.textviewClassificacao)
+        val result = intent.getFloatExtra("EXTRA_RESULT", 0.1f)
+
+        tvresult.text = result.toString()
+
+        var classificacao = ""
+        classificacao = if(result < 18.5f){
+            "ABAIXO DO PESO"
+        }else if(result in 18.5f..24.999f){
+            "NORMAL"
+        }else if(result in 25f..29.999f){
+            "SOBREPESO"
+        }else if(result in 30f..39.999f){
+            "OBESIDADE"
+        }else{
+            "OBESIDADE GRAVE"
+        }
+
+        tvClassificacao.text = getString(R.string.message_classificacao, classificacao)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        finish()
+        return super.onOptionsItemSelected(item)
+    }
+}
